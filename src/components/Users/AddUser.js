c18811b32ba490b7617ec7921b6fccc6c2fa87c9
brettl1991 <brettl1991@gmail.com>
2023-03-23 15:47:17 +0000

    feat: add re-usable card component and styles via props

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,15 +1,21 @@
+import Card from "../UI/Card";
+
+import styles from "./AddUser.module.css";
+
 const AddUser = () => {
   const addUserHandle = (event) => {
     event.preventDefault();
   };
   return (
-    <form onSubmit={addUserHandle}>
-      <label htmlFor="username">User name</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandle}>
+        <label htmlFor="username">User name</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
